f1d68b8868ec720c5ab89e38fd9458ee59e56a62
PabloRafael-coder <pabloofael@gmail.com>
2023-12-03 14:53:50 -0300

    New alteration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,60 @@
-import React from 'react'
+import React from 'react';
+import Users from './assets/usuarios.webp';
+import Arrow from './assets/arrow-right.svg';
+import Trash from './assets/trash-2.svg'
+
+import { Container, Image, ContainerItens, H1, Label, Input, Button, User } from './style'
+
+
 
 const App = () => {
-  return <div>Hello World</div>
+
+  const users = [
+    { id: Math.random(), name: 'Pablo', age: 23 },
+    { id: Math.random(), name: 'Rafael', age: 30 },
+  ];
+
+  return (
+    <Container>
+      <Image src={Users} width='300px' />
+      <ContainerItens>
+        <H1>
+          Cadastramento de Usuários
+        </H1>
+        <Label for='name'>
+          Nome:
+        </Label>
+        <Input name='name' placeholder='Nome'>
+
+        </Input>
+        <Label for='idade'>
+          Idade:
+        </Label>
+        <Input name='idade' placeholder='Idade'>
+
+        </Input>
+
+        <Button >
+          Cadastrar
+
+          <Image src={Arrow} />
+        </Button>
+
+        <ul>
+          {
+            users.map((user) => (
+              <User key={user.id}>
+                <p>{user.name}</p>
+                <p>{user.age}</p>
+                <Image src={Trash}/>
+              </User>
+            ))
+          }
+        </ul>
+      </ContainerItens>
+    </Container>
+  );
+
 }
 
-export default App
+export default App;
---
+++ src/style.js
@@ -0,0 +1,84 @@
+import styled from 'styled-components';
+
+
+
+export const Container = styled.div`
+    background-color: gainsboro;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+
+`;
+
+export const Image = styled.img`
+`;
+
+export const ContainerItens = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 30px;
+    width: 50%;
+    background-color: rgb(254,254,255);
+    border-radius: 10px;
+    gap: 10px;
+    padding: 50px 36px;
+`;
+
+export const H1 = styled.h1`
+    text-transform: uppercase;
+`;
+
+export const Label = styled.label`
+    font-size: 20px;
+    letter-spacing: 0.5px;
+    font-weight: 700;
+`;
+
+export const Input = styled.input`
+    max-width: 70%;
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+    font-size: 16px;
+    background-color: gainsboro;
+
+`;
+
+export const Button = styled.button`
+    width: 40%;
+    font-size: 16px;
+    font-weight: 700;
+    background-color: rgb(115,103,240);
+    border: none;
+    border-radius: 5px;
+    padding: 15px;
+    cursor: pointer;
+
+    &:hover {
+        opacity: 0.8;
+    }
+`;
+
+export const User = styled.li`
+    
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    
+    width: 70%;
+    padding: 10px;
+    margin-top: 20px;
+
+    font-size: 16px;
+
+    border: none;
+    border-radius: 5px;
+
+    background-color: gainsboro;
+
+    p {
+        font-size: 16px;
+        font-weight: 700;
+    }
+`;
